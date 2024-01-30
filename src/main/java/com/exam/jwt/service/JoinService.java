babe170c392943638b4bb9132ea68ef70abc6c3c
Project-DAL <drinkallliquor@gmail.com>
2024-01-30 18:30:50 +0900

    Initial commit

---
+++ src/main/java/com/exam/jwt/service/JoinService.java
@@ -0,0 +1,38 @@
+package com.exam.jwt.service;
+
+import com.exam.jwt.dto.JoinDTO;
+import com.exam.jwt.entity.UserEntity;
+import com.exam.jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JoinService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    public void joinProcess(JoinDTO joinDTO) {
+
+        String username = joinDTO.getUsername();
+        String password = joinDTO.getPassword();
+
+        Boolean isExist = userRepository.existsByUsername(username);
+
+        if (isExist) {
+
+            return;
+        }
+
+        UserEntity data = new UserEntity();
+
+        data.setUsername(username);
+        data.setPassword(bCryptPasswordEncoder.encode(password));
+        data.setRole("ROLE_ADMIN");
+
+        userRepository.save(data);
+    }
+}
